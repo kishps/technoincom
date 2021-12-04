c79319aa546694b25111e24ff3a258373bb929bc
kishps <kishps1@gmail.com>
2021-12-04 15:10:40 +0300

    ##

--- report/.parameters.php
+++
@@ -1,8 +0,0 @@
-<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-$arComponentParameters = [
-    'GROUPS'     => [],
-    'PARAMETERS' => [
-        //'CACHE_TIME' => ['DEFAULT' => 36000000],
-    ],
-];
